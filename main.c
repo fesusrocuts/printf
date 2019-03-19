d2ee80f212d1047ef6c26e637107020838ac6b1d
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-19 05:03:33 +0000

    [change] update files to run into pointer funtion the va_list, the func %s, %c, %c y %i is working

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-_printf("Hola soy %s, la inicial de mi nombre es:  %d, la hexa es %c", "Fesus", 74, 'H');
+_printf("Hola soy %s, la inicial de mi nombre es:  %d, la hexa es %c, tengo %i agnos", "Fesus", 74, 'H', 36);
 /*_printf("Character:[%c]\n", 'H');
 printf("Character:[%c]\n", 'H');
 _printf("String:[%s]\n", "I am a string !");
