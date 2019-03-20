3cbcfd450db8ded72698ffdcf7c853e8fed5f839
GaviriaAmaya <judamayaga@unal.edu.co>
2019-03-20 04:47:44 +0000

    Removed altered _printf.c Test with default _printf function

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include "holberton.h"
 
@@ -8,11 +9,26 @@
  */
 int main(void)
 {
-_printf("Hola soy %s, la inicial de mi nombre es:  %d, la hexa es %c, tengo %i agnos", "Fesus", 74, 'H', 36);
-/*_printf("Character:[%c]\n", 'H');
-printf("Character:[%c]\n", 'H');
-_printf("String:[%s]\n", "I am a string !");
-printf("String:[%s]\n", "I am a string !");
-*/
-return (0);
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
+	_printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	_printf("Unknown:[%r]\n");
+	return (0);
 }
