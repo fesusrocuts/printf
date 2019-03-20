ebe5637bb721608868cf5695d45ddcc5a0eb4bcd
GaviriaAmaya <judamayaga@unal.edu.co>
2019-03-20 04:04:52 +0000

    [changes] Declaring the structure and typedef on the header file. Adding descriptions to the functions

--- holberton.h
+++ holberton.h
@@ -4,9 +4,20 @@ int _putchar(char c);
 int _pow(int base, int exp);
 void print_number(int n);
 int _printf(const char *format, ...);
+/**
+ *struct st - Contains the prototyped functions to the conversion spec
+ *@st: Argument received
+ *@f: Pointer to the associated function
+ */
+typedef struct st
+{
+	char *st;
+	int (*f)(va_list);
+} st_t;
 int (*valformat(char *))(va_list);
 int pt_s(va_list);
 int pt_c(va_list);
 int pt_d(va_list);
 int pt_i(va_list);
+
 #endif
