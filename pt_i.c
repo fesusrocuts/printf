8e681e25d5b882fb23c8a4184346219d329f971b
GaviriaAmaya <judamayaga@unal.edu.co>
2019-03-20 03:25:31 +0000

    [feature] Changes on the description in function files

--- pt_i.c
+++ pt_i.c
@@ -3,13 +3,16 @@
 #include <string.h>
 #include <stdarg.h>
 #include "holberton.h"
-
+/**
+ *pt_i - Converts the argument into an integer
+ *@args: Argument received through the main function
+ *Return: Always success - 0
+ */
 int pt_i(va_list args)
 {
 	int i = 0;
 	int n = va_arg(args, int);
-	
+
 	print_number(n);
 	return (i);
 }
-
