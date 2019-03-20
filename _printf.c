66dc8206af814b91cbe7b47c327c171efce9aa0f
GaviriaAmaya <judamayaga@unal.edu.co>
2019-03-20 02:58:14 +0000

    [feature] Changes on the spaces and variable declarations. Adding description to the function

--- _printf.c
+++ _printf.c
@@ -4,86 +4,55 @@
 #include <string.h>
 #include <stdarg.h>
 #include "holberton.h"
-
-
+/**
+ *_printf - Formats an argument and then prints it on the std output
+ *@format: String tha contains the arguments of the functions
+ *Return: String lenght without the NULL character
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0;
-	char str[255];
-	char str2[16];
-	char *buffer;
-	int cbuffer = 0;
-	int newl = 0;
-	int reg = 0;
-	char *specialchar = "0123456789.%cdefigs";
-	int ch = 0;
-	int subreg = 0;
-	/*using before backslahes to reg special char*/
-	/*char *specialchar2 = "a\br\"ftn0'v?";*/
-	/*the . should can strong parse*/
+	char str[255], str2[16], *buffer, *specialchar = "0123456789.%cdefigs";
+	int subreg = 0, cbuffer = 0, newl = 0, reg = 0, ch = 0, i = 0;
 
 	va_start(args, format);
-	strcpy(str,format);
-	
+	strcpy(str, format);
 	buffer = calloc(16, sizeof(char));
-	if(buffer == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
-
+	if (buffer == NULL)
+		{free(buffer);
+		return (0); }
 	while (str[i])
-	{
-		if(reg == 1)
 		{
-			while(specialchar[ch])
+		if (reg == 1)
 			{
-				if(specialchar[ch] == str[i])
-					subreg = 1;
-				ch++;
-			}
-			ch = 0;
+			while (specialchar[ch])
+				{
+				if (specialchar[ch] == str[i])
+				subreg = 1;
+				ch++;}
+		        ch = 0;
 			if (subreg == 0)
-			{
-				reg = 0;
+				{reg = 0;
 				cbuffer = 0;
 				strcpy(str2, buffer);
-				/*printf("\find: %s\n", str2);*/
-				/*here is the magic*/
-				newl+=(*valformat(str2))(args);
-			}
+				newl += (*valformat(str2))(args); }
 			else
-			{
-				/*printf("\nadd to buffer: %c\n", str[i]);*/
-				*(buffer + cbuffer) = str[i];
-				cbuffer++;
-			}
-			subreg = 0;
-		}
+				{*(buffer + cbuffer) = str[i];
+				cbuffer++; }
+			subreg = 0; }
 		if (str[i] == '%' && reg == 0)
-		{
-			reg = 1;
+			{reg = 1;
 			*(buffer + cbuffer) = str[i];
-			cbuffer++;
-		}
-		if(reg == 0)
-		{
-			_putchar(str[i]);
-			newl++;
-		}
-		i++;
-	}
-	/*print end buffer when end text with reg==1*/
+			cbuffer++; }
+		if (reg == 0)
+			{_putchar(str[i]);
+			newl++;	}
+		i++;}
 	if (reg == 1)
-	{
-		reg = 0;
+		{reg = 0;
 		cbuffer = 0;
 		strcpy(str2, buffer);
-		/*printf("\nfind: %s\n", str2);*/
-		/*here is the magic*/
-		newl+=(*valformat(str2))(args);
-	}
+		newl += (*valformat(str2))(args); }
 	va_end(args);
 	putchar(10);
 	return (newl);
